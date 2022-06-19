ff5d00daf68261394dd487b477b0152daa83dc42
Joe Grant <jmgrant1995@gmail.com>
2022-06-19 10:39:10 +0800

    add backlink to halfcab

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -35,7 +35,7 @@ const BottomBar = () => {
     <footer className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="mt-8 md:mt-0">
-          <p className="text-center text-base text-gray-400">&copy; { new Date().getFullYear() } Physi-Go-Therapy. All rights reserved.</p>
+          <p className="text-center text-base text-gray-400">&copy; { new Date().getFullYear() } Physi-Go-Therapy. All rights reserved. Built by <a className="hover:text-purple" href="https://halfcab.com.au" target="_blank">Halfcab.</a></p>
           <div className="flex justify-center md:justify-start mt-3 space-x-3">
             {navigation.map((item) => (
               <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
